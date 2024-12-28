2a77ddd4a5d550f8c4613ef559d37938b252a900
Elvy1999 <elvydecoo99@gmail.com>
2024-12-27 21:25:36 -0500

    added click functionality to the buttons

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ const buttonDivide = document.getElementById("/");
 const buttonMultiply = document.getElementById("*");
 const buttonMinus = document.getElementById("-");
 const buttonPlus = document.getElementById("+");
+
+const input = document.getElementById("input");
+const clear = document.getElementById("clear");
+const result = document.getElementById("result");
+
+let multiply = (num1, num2) => num1 * num2;
+let divide = (num1, num2) => num1 / num2;
+let add = (num1, num2) => num1 + num2;
+let subtract = (num1, num2) => num1 - num2;
+
+let operate = (num1, num2, operator) => {
+  if (operator == "*") return multiply(num1, num2);
+  if (operator == "/") return divide(num1, num2);
+  if (operator == "+") return add(num1, num2);
+  if (operator == "-") return subtract(num1, num2);
+};
+
+for (let i = 0; i < 10; i++) {
+  const button = document.getElementById(i.toString());
+  button.addEventListener("click", () => {
+    input.innerHTML += i;
+  });
+}
+
+clear.addEventListener("click", () => {
+  input.innerHTML = "";
+  result.innerHTML = "";
+});
